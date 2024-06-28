1a7f95f71018d82eae551e200bded6a66100c354
Shreyansh Sinha <shreyanshsinha@192.168.1.2>
2024-06-28 23:58:28 +0530

    remove unused code

--- word-count/Story2/pkg/wc.go
+++ word-count/Story2/pkg/wc.go
@@ -9,44 +9,6 @@ import (
 	common "example.com/common/utils"
 )
 
-// check if there exists a file with given parameter
-func isFile(param string) bool {
-
-	fileInfo, err := os.Stat(param)
-	if err != nil {
-		return false
-	}
-
-	if fileInfo.Mode().IsRegular() {
-		return true
-	}
-
-	return false
-
-}
-
-// check if file with given parameter has read permissions
-func checkFileReadPermissions(param string) bool {
-
-	fileInfo, err := os.Stat(param)
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	if fileInfo.Mode().Perm()&0400 != 0 {
-		return true
-	}
-
-	return false
-}
-
-// check if file or directory exists
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return !os.IsNotExist(err)
-}
-
 func WordsCount(param string) {
 
 	if !common.FileExists(param) {
